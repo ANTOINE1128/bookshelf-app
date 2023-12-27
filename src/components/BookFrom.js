a1cde0b1f6223563cd34c9a9814fa3e62a203f9e
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-27 21:08:54 +0200

    Create book-form

--- src/components/BookFrom.js
+++ src/components/BookFrom.js
@@ -1,10 +1,42 @@
-import React from "react";
-const BookFrom = () => {
-    return ( 
-         <div>
-<h2>book form</h2>
+// src/components/BookForm.js
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../booksSlice';
+
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = () => {
+    // Generate a unique ID (you can use a library like uuid)
+    const id = Math.random().toString(36).substr(2, 9);
+
+    dispatch(addBook({ id, title, author, category }));
+    setTitle('');
+    setAuthor('');
+    setCategory('');
+  };
+
+  return (
+    <div>
+      <h2>Add New Book</h2>
+      <div>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <div>
+        <label>Author:</label>
+        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+      </div>
+      <div>
+        <label>Category:</label>
+        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+      </div>
+      <button onClick={handleSubmit}>Add Book</button>
     </div>
-    );
-}
- 
-export default BookFrom;
+  );
+};
+
+export default BookForm;
